01ee78ae685e6c9eea3bdcea1325f77197781573
MelnykY <106350059+MelnykY@users.noreply.github.com>
2022-12-25 00:18:54 +0100

    repo settings

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,44 +6,23 @@ console.log(galleryItems);
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
+// const gallery = document.querySelector(".gallery");
+// const galleryEl = galleryItems
+//   .map((item) => {
+//     return `<div>
+//     <a class="gallery__item" href="${item.original}">
+//     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+// </a>
+//     </div>`;
+//   })
+//   .join("");
+// gallery.insertAdjacentHTML("beforeend", galleryEl);
 
+// var lightbox = new SimpleLightbox(".gallery a", {
+//   captions: true,
+//   captionsData: "alt",
+//   captionPosition: "bottom",
+//   captionDelay: 250,
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const gallery = document.querySelector(".gallery");
-const galleryEl = galleryItems
-  .map((item) => {
-    return `<div>
-    <a class="gallery__item" href="${item.original}">
-    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>
-    </div>`;
-  })
-  .join("");
-gallery.insertAdjacentHTML("beforeend", galleryEl);
-
-var lightbox = new SimpleLightbox(".gallery a", {
-  captions: true,
-  captionsData: "alt",
-  captionPosition: "bottom",
-  captionDelay: 250,
-});
-
-console.log(galleryItems);
+// console.log(galleryItems);
